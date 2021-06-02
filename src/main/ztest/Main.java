9ca3e63c935cddda439cc70714f92dc0b2fa5aa3
wangzhigang <15105182362@163.com>
2021-06-02 16:25:16 +0800

    搭建jdk8源码阅读环境

---
+++ src/main/ztest/Main.java
@@ -0,0 +1,12 @@
+package ztest;
+
+/**
+ * @Author 08011960
+ * @Date 2021/6/2 16:16
+ * @Description
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
